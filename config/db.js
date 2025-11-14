6233312191604e5cc9719f8aa303061cbe5294c7
mardonbekhursanov <mardonbekhursanov91@gmail.com>
2025-11-14 18:40:52 +0500

    .env qo'shildi

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 const Pool = require("pg").Pool
-
+const connectionString = process.env.DATABASE_URL || `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 const pool = new Pool({
     user: "postgres",
     password: "mardon03",
     port: 5432,
     host: "localhost",
     database: "market",
+    connectionString,
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 })
 
 module.exports = pool;

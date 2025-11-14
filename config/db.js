f134072a50d938528c22200172a2fc3b27134819
mardonbekhursanov <mardonbekhursanov91@gmail.com>
2025-11-14 19:15:45 +0500

    db yana

--- config/db.js
+++ config/db.js
@@ -1,15 +1,12 @@
-const Pool = require("pg").Pool
+const { Pool } = require("pg");
+
 const connectionString = process.env.DATABASE_URL || "postgres://postgres:mardon03@localhost:5432/market";
 
 if (!connectionString) throw new Error("DATABASE_URL not set");
+
 const pool = new Pool({
-    user: "postgres",
-    password: "mardon03",
-    port: 5432,
-    host: "localhost",
-    database: "market",
     connectionString,
     ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
-})
+});
 
 module.exports = pool;
